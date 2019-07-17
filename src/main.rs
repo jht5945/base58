d35ca9c06ea0eff8767d0011306fbf1fcf8b63a9
Hatter Jiang@Pixelbook <jht5945@gmail.com>
2019-07-18 07:53:50 +0800

    add print_usage()

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 
+fn print_usage() {
+    print!(r#"Usage: base58 [OPTION]... [FILE]
+Base58 encode or decode FILE, or standard input, to standard output.
+
+With no FILE, or when FILE is -, read standard input.
+"#);
+}
+
 fn main() {
+    print_usage();
     println!("hello world, base58");
 }
 
