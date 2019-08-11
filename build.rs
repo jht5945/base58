71dbfd219c5ad6c5b67e7aad53d5d2cf0dcb14e9
Hatter Jiang@Pixelbook <jht5945@gmail.com>
2019-08-11 13:10:38 +0800

    update crates

--- build.rs
+++ build.rs
@@ -1,7 +1,9 @@
 use std::process::Command;
 fn main() {
-    // note: add error checking yourself.
     let output = Command::new("git").args(&["rev-parse", "HEAD"]).output().unwrap();
     let git_hash = String::from_utf8(output.stdout).unwrap();
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
+    let date_output = Command::new("date").args(&["+%Y-%m-%dT%H:%M:%S%z"]).output().unwrap();
+    let date = String::from_utf8(date_output.stdout).unwrap();
+    println!("cargo:rustc-env=BUILD_DATE={}", date);
 }
--- src/main.rs
+++ src/main.rs
@@ -14,22 +14,32 @@ use std::{
 use base58::{ToBase58, FromBase58};
 
 use opt::*;
-use rust_util::*;
+use rust_util::{
+    util_msg::*,
+};
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 const GIT_HASH: &str = env!("GIT_HASH");
+const BUILD_DATE: &str = env!("BUILD_DATE");
 
-fn print_version() {
+fn print_version(options: &Options) {
     print!(r#"base58 {} - {}
 Copyright (C) 2019 Hatter Jiang.
 License MIT <https://opensource.org/licenses/MIT>
 
 Written by Hatter Jiang
 "#, VERSION, &GIT_HASH[0..7]);
+    if options.verbose {
+        print_message(MessageType::DEBUG, &format!("Full GIT_HASH: {}", GIT_HASH));
+        print_message(MessageType::DEBUG, &format!("Build date: {}", BUILD_DATE));
+    }
 }
 
-fn encode_base58(read: &mut Read, new_line: bool) {
+fn encode_base58(read: &mut Read, options: &Options) {
     let mut buffer = Vec::new();
+    if options.verbose {
+        print_message(MessageType::DEBUG, "Start read input.");
+    }
     match read.read_to_end(&mut buffer) {
         Err(err) => {
             print_message(MessageType::ERROR, &format!("Read from stdin failed: {}", err));
@@ -37,10 +47,13 @@ fn encode_base58(read: &mut Read, new_line: bool) {
         },
         Ok(_) => (),
     };
-    print!("{}{}", &buffer.to_base58(), match new_line { false => "", true => "\n", });
+    if options.verbose {
+        print_message(MessageType::DEBUG, "Read input finished.");
+    }
+    print!("{}{}", &buffer.to_base58(), match options.new_line { false => "", true => "\n", });
 }
 
-fn decode_base58(read: &mut Read, token: &str, new_line: bool, verbose: bool) {
+fn decode_base58(read: &mut Read, token: &str, options: &Options) {
     let mut buffer = String::new();
     match read.read_to_string(&mut buffer) {
         Err(err) => {
@@ -49,7 +62,7 @@ fn decode_base58(read: &mut Read, token: &str, new_line: bool, verbose: bool) {
         },
         Ok(_) => (),
     };
-    if verbose {
+    if options.verbose {
         print_message(MessageType::INFO, &format!("Read content: {}", &buffer));
     }
     match buffer.as_str().trim().from_base58() {
@@ -59,7 +72,7 @@ fn decode_base58(read: &mut Read, token: &str, new_line: bool, verbose: bool) {
         },
         Ok(bs) => {
             io::stdout().write(bs.as_slice()).unwrap();
-            if new_line {
+            if options.new_line {
                 println!();
             } else {
                 io::stdout().flush().unwrap();
@@ -69,12 +82,11 @@ fn decode_base58(read: &mut Read, token: &str, new_line: bool, verbose: bool) {
 }
 
 
-
 fn main() {
     let options = Options::new_and_parse_args();
     
     if options.version {
-        print_version();
+        print_version(&options);
         return;
     }
 
@@ -87,15 +99,15 @@ fn main() {
             Ok(f) => f,
         };
         if options.decode {
-            decode_base58(&mut f, &format!("file: {}", &options.file), options.new_line, options.verbose);
+            decode_base58(&mut f, &format!("file: {}", &options.file), &options);
         } else {
-            encode_base58(&mut f, options.new_line);
+            encode_base58(&mut f, &options);
         }
     } else {
         if options.decode {
-            decode_base58(&mut io::stdin(), "stdin", options.new_line, options.verbose);
+            decode_base58(&mut io::stdin(), "stdin", &options);
         } else {
-            encode_base58(&mut io::stdin(), options.new_line);
+            encode_base58(&mut io::stdin(), &options);
         }
     }
 }
